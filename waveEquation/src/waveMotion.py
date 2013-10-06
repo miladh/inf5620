319b925e63dd08a4db17d9a76c4b110af36d5b81
Milad Hobbi Mobarhan <miladh@fys.uio.no>
2013-10-06 11:50:10 +0200

    truncation error included in report

--- waveEquation/src/waveMotion.py
+++ waveEquation/src/waveMotion.py
@@ -5,11 +5,13 @@ Created on Fri Oct  4 11:46:19 2013
 @author: Milad H. Mobarhan
 """
 from pylab import*
+import numpy as np
+import sys
 close("all")
     
 "*****************************************************************************"
 def solver(problem, Lx, Ly, Nx, Ny, dt, T, 
-           user_action=None, version ='scalar',
+           user_action=None, version ='vec',
            BC = 'dirichlet'):
     """
     Solve 
@@ -20,7 +22,7 @@ def solver(problem, Lx, Ly, Nx, Ny, dt, T,
     """
     if version == 'scalar':
         advance = advance_scalar
-    elif version == 'vectorized': 
+    elif version == 'vec': 
         advance = advance_vectorized
         
     if BC == 'dirichlet':
@@ -28,10 +30,12 @@ def solver(problem, Lx, Ly, Nx, Ny, dt, T,
     elif BC == 'neumann': 
         BC_type = neumann_BC
     
-    x = linspace(0, Lx, Nx+1)  # mesh points in x dir
-    y = linspace(0, Ly, Ny+1)  # mesh points in y dir
-    dx = x[1] - x[0]           # mesh spacing in x dir
-    dy = y[1] - y[0]           # mesh spacing in y dir
+    x = linspace(0, Lx, Nx+1)         # mesh points 
+    y = linspace(0, Ly, Ny+1)         
+    dx = x[1] - x[0]                  # mesh spacing
+    dy = y[1] - y[0]                
+    x = sort(append(x, [-dx, Lx+dx])) # Adding ghost points
+    y = sort(append(y, [-dy, Ly+dy])) 
     
     
     Nt = int(round(T/float(dt)))
@@ -42,13 +46,12 @@ def solver(problem, Lx, Ly, Nx, Ny, dt, T,
     dtdy2 = (dt/dy)**2     # help variable
         
 
-    u   = zeros((Nx+1,Ny+1))   # solution array
-    u_1 = zeros((Nx+1,Ny+1))   # solution at t-dt
-    u_2 = zeros((Nx+1,Ny+1))   # solution at t-2*dt
-
-    print u.shape, u.shape[1]
-    Ix = range(0, u.shape[0])
-    Iy = range(0, u.shape[1])
+    u   = zeros((Nx+3,Ny+3))   # solution array
+    u_1 = zeros((Nx+3,Ny+3))   # solution at t-dt
+    u_2 = zeros((Nx+3,Ny+3))   # solution at t-2*dt
+    
+    Ix = range(1, u.shape[0]-1) #indices for the real physical points
+    Iy = range(1, u.shape[1]-1)
     It = range(0, t.shape[0])    
     
     import time; t0 = time.clock()          # for measuring CPU time    
@@ -56,7 +59,9 @@ def solver(problem, Lx, Ly, Nx, Ny, dt, T,
     # Load initial condition into u_1
     for i in Ix:
         for j in Iy: 
-            u_1[i,j] = problem.I(x[i], y[j])    
+            u_1[i,j] = problem.I(x[i], y[j])  
+    # set ghost values
+    u_1 = BC_type(u_1, Ix, Iy)
             
     if user_action is not None:
         user_action(u_1, x, y, t[0])
@@ -65,7 +70,7 @@ def solver(problem, Lx, Ly, Nx, Ny, dt, T,
     # Special formula for first time step
     u = advance(problem, u, u_1, u_2, x, y, t[0], dtdx2, dtdy2, dt, 
                 step1=True, setBC = BC_type)
-
+        
     if user_action is not None:
         user_action(u, x, y, t[1])
 
@@ -82,9 +87,32 @@ def solver(problem, Lx, Ly, Nx, Ny, dt, T,
         u_2[:,:], u_1[:,:] = u_1, u
 
 
-    cpu_time = t0 - time.clock() 
+    cpu_time = time.clock() - t0
     
-    return u, x, y, t, cpu_time
+    return u[1:-1,1:-1], x, y, t, cpu_time
+    
+"*****************************************************************************"
+def neumann_BC(u,Ix,Iy):
+    """
+    Set Neumann boundary conditions
+    """
+    i = Ix[0]          # x=0 boundary
+    for j in range(0,u.shape[1]): 
+        u[i-1,j] = u[i+1,j]
+    
+    i = Ix[-1]         # x=Lx boundary
+    for j in range(0,u.shape[1]): 
+        u[i+1,j] = u[i-1,j]
+    
+    j = Iy[0]          # y=0 boundary
+    for i in range(0,u.shape[0]): 
+        u[i,j-1] = u[i,j+1]
+    
+    j = Iy[-1]         # y=Ly boundary
+    for i in range(0,u.shape[0]): 
+        u[i,j+1] = u[i,j-1]
+    
+    return u
 "*****************************************************************************"
 def dirichlet_BC(u,Ix,Iy):
     """
@@ -103,33 +131,14 @@ def dirichlet_BC(u,Ix,Iy):
     for i in Ix: u[i,j] = 0
     
     return u
-    
-"*****************************************************************************"
-def neumann_BC(u,Ix,Iy):
-    """
-    Set Neumann boundary conditions
-    """
-    i = Ix[0]          # x=0 boundary
-    for j in Iy: u[i-1,j] = u[i+1,j]
-    
-    i = Ix[-1]         # x=Lx boundary
-    for j in Iy: u[i+1,j] = u[i-1,j]
-    
-    i = Iy[0]          # y=0 boundary
-    for i in Ix: u[i,j-1] = u[i,j+1]
-    
-    i = Iy[-1]         # y=Ly boundary
-    for i in Ix: u[i,j+1] = u[i,j-1]
-    
-    return u
-    
-"*****************************************************************************"
 
+    
+"*****************************************************************************"
 def advance_scalar(problem, u, u_1, u_2, x, y, t, 
-            dtdx2, dtdy2, dt, step1=False, setBC = dirichlet_BC):
+            dtdx2, dtdy2, dt, step1=False, setBC = None):
                        
-    Ix = range(0, u.shape[0]);  
-    Iy = range(0, u.shape[1])
+    Ix = range(1, u.shape[0]-1)
+    Iy = range(1, u.shape[1]-1)
     dt2 = dt**2
     
     if step1:
@@ -137,8 +146,8 @@ def advance_scalar(problem, u, u_1, u_2, x, y, t,
     else:
         D1 = 1.0; D2=0.0      
         
-    for i in Ix[1:-1]:
-        for j in Iy[1:-1]:
+    for i in Ix:
+        for j in Iy:
             qij = problem.q(x[i], y[j])
             qpi = (problem.q(x[i+1], y[j]) + qij)*0.5
             qmi = (qij + problem.q(x[i-1], y[j]))*0.5
@@ -156,7 +165,7 @@ def advance_scalar(problem, u, u_1, u_2, x, y, t,
                  (D1*u_2[i,j] - D2*2*dt*problem.V(x[i], y[j]))*(fac-1) +\
                  dt2/pij *problem.f(x[i], y[j], t) +\
                  dtdx2/pij * u_x + dtdy2/pij * u_y
-                     
+    
     if step1:
         u /= ((1+fac)*(2-fac))
     else:
@@ -164,14 +173,14 @@ def advance_scalar(problem, u, u_1, u_2, x, y, t,
     
     # Set Boundary conditions
     u = setBC(u,Ix,Iy)   
-    
+
     return u
 "*****************************************************************************"
 def advance_vectorized(problem, u, u_1, u_2, x, y, t, 
-            dtdx2, dtdy2, dt, step1=False, setBC=None):
-                       
-    Ix = range(0, u.shape[0]);  
-    Iy = range(0, u.shape[1])
+            dtdx2, dtdy2, dt, step1=False, setBC = None):
+       
+    Ix = range(1, u.shape[0]-1)
+    Iy = range(1, u.shape[1]-1)               
     dt2 = dt**2
     
     if step1:
@@ -179,26 +188,25 @@ def advance_vectorized(problem, u, u_1, u_2, x, y, t,
     else:
         D1 = 1.0; D2=0.0      
         
-    for i in Ix[1:-1]:
-        for j in Iy[1:-1]:
-            qij = problem.q(x[i], y[j])
-            qpi = (problem.q(x[i+1], y[j]) + qij)*0.5
-            qmi = (qij + problem.q(x[i-1], y[j]))*0.5
-            qpj = (problem.q(x[i], y[j+1]) + qij)*0.5
-            qmj = (qij + problem.q(x[i], y[j-1]))*0.5
-                        
-            uij = u_1[i,j]
-            u_x = qpi*(u_1[i+1,j] - uij) - qmi*(uij - u_1[i-1,j])
-            u_y = qpj*(u_1[i,j+1] - uij) - qmj*(uij - u_1[i,j-1])
-            
-            pij = problem.p(x[i], y[j])
-            fac = problem.b*float(dt) / (2*pij)
-            
-            u[i,j] = 2*u_1[i,j]+\
-                 (D1*u_2[i,j] - D2*2*dt*problem.V(x[i], y[j]))*(fac-1) +\
-                 dt2/pij *problem.f(x[i], y[j], t) +\
-                 dtdx2/pij * u_x + dtdy2/pij * u_y
-                     
+    qij = problem.q(x[1:-1], y[1:-1])
+    qpi = (problem.q( x[2:], y[1:-1]) + qij)*0.5
+    qmi = (qij + problem.q(x[:-2], y[1:-1]))*0.5
+    qpj = (problem.q( x[1:-1], y[2:]) + qij)*0.5
+    qmj = (qij + problem.q(x[1:-1], y[:-2]))*0.5
+                
+    uij = u_1[1:-1,1:-1] 
+    u_x = qpi*(u_1[2:,1:-1] - uij) - qmi*(uij - u_1[:-2,1:-1])
+    u_y = qpj*(u_1[1:-1,2:] - uij) - qmj*(uij - u_1[1:-1,:-2])
+    
+    pij = problem.p(x[1:-1], y[1:-1])
+    fac = problem.b*float(dt)/(2*pij)
+    
+    
+    u[1:-1,1:-1] = 2*u_1[1:-1,1:-1]+\
+         (D1*u_2[1:-1,1:-1] - D2*2*dt*problem.V(x[1:-1], y[1:-1]))*\
+         (fac-1) +dt2/pij *problem.f(x[1:-1], y[1:-1], t) +\
+         dtdx2/pij * u_x + dtdy2/pij * u_y
+                      
     if step1:
         u /= ((1+fac)*(2-fac))
     else:
@@ -208,7 +216,6 @@ def advance_vectorized(problem, u, u_1, u_2, x, y, t,
     u = setBC(u,Ix,Iy)   
 
     return u
-
 "*****************************************************************************"
 def viz(problem, Lx, Ly, Nx, Ny, dt, T, animate=True):
     """
@@ -223,10 +230,10 @@ def viz(problem, Lx, Ly, Nx, Ny, dt, T, animate=True):
         """
         user_action function for solver.
         """
-        X, Y = meshgrid(x, y)
+        X, Y = meshgrid(x[1:-1], y[1:-1])
         fig.clf()
         ax = fig.gca(projection='3d')
-        ax.plot_wireframe(X, Y, u,rstride=1, cstride=1, cmap=cm.jet,
+        ax.plot_wireframe(X, Y, u[1:-1,1:-1],rstride=1, cstride=1, cmap=cm.jet,
                         linewidth=0.1, antialiased=False)
         
         ax.set_zlim(-0.1,0.1)
@@ -246,7 +253,11 @@ def viz(problem, Lx, Ly, Nx, Ny, dt, T, animate=True):
 
     
     user_action = plot_u if animate else None
-    u, x, t, cpu = solver(problem, Lx, Ly, Nx, Ny, dt, T, user_action)
+    u, x,y, t, cpu = solver(problem, Lx, Ly, Nx, Ny, dt, T, user_action)
+
+    print "CPU time: ", cpu ,"\n"
+    
+    
 
 "*****************************************************************************"
 class Problem():
@@ -291,7 +302,7 @@ class SimpleWave(Problem):
         return 0.0;
       
     def V(self,x,y):     
-        return sin(x)*sin(y)
+        return sin(x)*cos(y)
 
     def f(self,x,y,t):
         return 0.0
@@ -317,11 +328,11 @@ def define_command_line_options(parser=None):
         default=1.0, help='upper boundary in y direction',metavar='Ly')
     
     parser.add_argument(
-        '--Nx', '--num_mesh_cells_x', type=int, default=30.0,
+        '--Nx', '--num_mesh_cells_x', type=int, default=5.0,
         help='total number of mesh cells in the x direction',metavar='Lx')
     
     parser.add_argument(
-        '--Ny', '--num_mesh_cells_y', type=int, default=30.0, 
+        '--Ny', '--num_mesh_cells_y', type=int, default=5.0, 
         help='total number of mesh cells in the y direction',metavar='Ly')   
    
     parser.add_argument(
@@ -329,7 +340,7 @@ def define_command_line_options(parser=None):
         help='time step value', metavar='dt')
         
     parser.add_argument(
-        '--T', '--stop_time', type=float, default=10.0,
+        '--T', '--stop_time', type=float, default=1.0,
         help='end time of simulation', metavar='T')    
     
     parser.add_argument(
